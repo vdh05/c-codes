21d0e566014040a267dfc2fdffa78cf1f6ea521f
Vidhi <harwanividhi05@gmail.com>
2022-11-27 18:28:17 +0530

    Added new files

---
+++ 1.1   new.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+printf("\n %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c  %c %c %c %c %c %c %c %c %c %c %c %c %c %c",1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3);
+printf("\n %c\t If u are resiting something,you are feeling it.\t\t %c",4,4);
+printf("\n%c\t Any energy you fight, you are feeding it.",3,3);
+printf("\n%c\t\tYou are inviting it to stay.\t\t\t%c",1,1);
+printf("\n %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c",1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3);
+printf("1");
+ return 0;
+ }
---
+++ 1.1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ void main()
+ { clrscr();
+ printf("\n %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c ",1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1);
+ printf("\n %c \"If you are resisting something, you are feeding it. \t \t %c",4,4); printf("\n %c \t Any energy you fight, you are feeding it. \t \t \t %c",3,3);
+ printf("\n %c \t \t If you are pushing something away, \t \t \t %c",1,1);
+ printf("\n %c \t \t \t You are inviting it to stay.\" by Michael Singer. %c", 4,4); printf("\n %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c ",1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1);
+ getch(); 
+} 
